3d0c90c038e3843f8d18cdffafa4cab00aebd6d1
George Nidekker <georgenidekker@gmail.com>
2024-06-02 15:05:26 +0300

    Сделаны дополнительные задания Блока 2

--- 2_files.py
+++ 2_files.py
@@ -10,13 +10,21 @@
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
-
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f: #Чтение содержимого файла
+        content = f.read()
+    
+    string_length = len(content) # Считаем длину строки
+    print(f"Длина строки: {string_length} символов")
+
+    word_count = len(content.split()) #Подсчет количества слов
+    print(f"Количеество слов: {word_count}")
+
+    modified_content = content.replace('.', '!') #Замена точек на восклицательные знаки
+
+    with open('referat2.txt', 'w', encoding='utf-8') as f:
+        f.write(modified_content)
+    print("Данные успешно обработаны и сохранены в файл referat2.txt")
 
 if __name__ == "__main__":
     main()
